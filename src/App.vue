1134c8f562aaf1bd4cda8399082deab0e273b328
oleg086rus <o.pototskiy@yandex.ru>
2022-12-13 23:45:10 +0500

    delete some files

--- src/App.vue
+++ src/App.vue
@@ -1,23 +1,12 @@
 <script setup>
-import HelloWorld from './components/HelloWorld.vue'
-import TheWelcome from './components/TheWelcome.vue'
 </script>
 
 <template>
-  <header>
-    <img alt="Vue logo" class="logo" src="./assets/logo.svg" width="125" height="125" />
-
-    <div class="wrapper">
-      <HelloWorld msg="You did it!" />
-    </div>
-  </header>
-
-  <main>
-    <TheWelcome />
-  </main>
+<h1>hello vue 3</h1>
 </template>
 
-<style scoped>
+<style lang="scss">
+
 header {
   line-height: 1.5;
 }
