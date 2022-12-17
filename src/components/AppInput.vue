b3a1a5a008e2b5c25ff5102bd8b324380dbc87ca
oleg086rus <o.pototskiy@yandex.ru>
2022-12-18 02:27:50 +0500

    working with forms, form and input components

---
+++ src/components/AppInput.vue
@@ -0,0 +1,40 @@
+<template>
+  <div class="form-control" :class="{invalid: error}">
+    <label :for="id">{{ label }}</label>
+    <input
+        type="text"
+        :id="id"
+        :placeholder="placeholder"
+        :value="modelValue"
+        @input="change"
+    >
+    <small v-if="error">{{ error }}</small>
+  </div>
+</template>
+
+<script>
+export default {
+  emits: ['update:value'],
+  name: 'AppInput',
+  props: {
+    modelValue: String,
+    error: String,
+    label: String,
+    placeholder: String
+  },
+  methods: {
+    change(event) {
+      this.$emit('update:value', event.target.value)
+    }
+  },
+  data() {
+    return {
+      id: 'input-' + Math.random()
+    }
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
