538ff7b80f6938de528b9cd4e00612918337be2e
oleg086rus <o.pototskiy@yandex.ru>
2022-12-20 11:59:44 +0500

    firebase, axios, get, post,delete requests

---
+++ src/components/http/AppPeopleList.vue
@@ -0,0 +1,28 @@
+<template>
+  <div v-if="people.length !== 0">
+    <div class="card inline" v-for="person in people" :key="person.id">
+      <h3>{{ person.firstName }}</h3>
+      <button class="btn danger" @click="$emit('remove', person.id)">Удалить</button>
+    </div>
+  </div>
+  <div class="card center" v-else>
+    <p>Людей пока нет</p>
+    <button class="btn" @click="$emit('load')">Загрузить список</button>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'AppPeopleList',
+  props: ['people'],
+  emits: ['load', 'remove']
+}
+</script>
+
+<style scoped>
+  .inline {
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+  }
+</style>
