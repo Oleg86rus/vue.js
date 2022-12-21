6c7fc5b16f21b0609a8b6a75e372c759c1b88bbe
oleg086rus <o.pototskiy@yandex.ru>
2022-12-22 01:37:54 +0500

    modificators

--- src/App.vue
+++ src/App.vue
@@ -1,23 +1,16 @@
 <template>
   <div class="container">
-    <div class="card">
-      <h2>Разговор про директивы</h2>
-      <div class="form-control">
-        <label for="inp">Активный по умолчанию</label>
-        <input type="text" id="inp" v-focus>
-      </div>
+    <div class="card" >
+
     </div>
   </div>
 </template>
 
 <script>
 
-import focusDirective from './focusDirective'
 
 export default {
-  directives: {
-    focus: focusDirective
-  }
+
 }
 </script>
 
---
+++ src/components/AppWithDirective.vue
@@ -0,0 +1,42 @@
+<template>
+  <div class="container">
+    <div class="card" v-if="show">
+      <h2 v-color:[type].blink.hover="myColor">Разговор про директивы</h2>
+      <div class="form-control">
+        <label for="inp">Активный по умолчанию</label>
+        <input type="text" id="inp" v-focus>
+      </div>
+      <button class="btn" @click="myColor = 'darkblue'">Сделать Синим</button>
+      <button class="btn" @click="type = type === 'color' ? 'backgroundColor' : 'color'">Переключить тип</button>
+    </div>
+  </div>
+</template>
+
+<script>
+
+import focusDirective from './focusDirective'
+import colorDirective from './colorDirective'
+
+export default {
+  directives: {
+    focus: focusDirective,
+    color: colorDirective
+  },
+  data() {
+    return {
+      myColor: 'darkred',
+      type: 'color',
+      show: true
+    }
+  },
+  mounted() {
+    // setTimeout(() => {
+    //   this.show = false
+    // }, 3000)
+  }
+}
+</script>
+
+<style>
+
+</style>
