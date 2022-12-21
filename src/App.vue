bd4add3fc41a0f893574552dd71e7752eb16d6b1
oleg086rus <o.pototskiy@yandex.ru>
2022-12-22 02:37:37 +0500

    add modal teleport

--- src/App.vue
+++ src/App.vue
@@ -1,16 +1,27 @@
 <template>
   <div class="container">
     <div class="card" >
-
+      <button class="btn primary" @click="modal = true">Открыть модалку</button>
+      <teleport to="body">
+        <app-modal
+            v-if="modal"
+            @close="modal = false"
+        ></app-modal>
+      </teleport>
     </div>
   </div>
 </template>
 
 <script>
-
+import AppModal from './components/AppModal.vue'
 
 export default {
-
+  components: {AppModal},
+  data() {
+    return {
+      modal: false
+    }
+  }
 }
 </script>
 
---
+++ src/components/AppModal.vue
@@ -0,0 +1,18 @@
+<template>
+  <div class="modal-backdrop" @click="$emit('close')"></div>
+  <div class="modal">
+    <h3>Это заголовок модалки</h3>
+    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Accusantium atque commodi inventore itaque minima, odit quae quasi tempore veniam voluptatibus?</p>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'AppModal',
+  emits: ['close']
+}
+</script>
+
+<style scoped>
+
+</style>
