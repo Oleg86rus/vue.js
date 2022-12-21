88625db8a4bd12cfeefec34560d01364f9358856
oleg086rus <o.pototskiy@yandex.ru>
2022-12-22 02:02:40 +0500

    create plugin

--- src/main.js
+++ src/main.js
@@ -1,11 +1,10 @@
 import { createApp } from 'vue'
 import App from './App.vue'
 import './theme.css'
+import translatePlugin from './translatePlugin'
 
-createApp(App)
-	// .directive('focus', {
-	// 	mounted(el) {
-	// 		el.focus()
-	// 	}
-	// })
-	.mount('#app')
+const app = createApp(App)
+
+app.use(translatePlugin)
+
+app.mount('#app')
