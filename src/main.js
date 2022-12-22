d29dbf05706f83eb78613c3a4ba89cfaff601cf5
oleg086rus <o.pototskiy@yandex.ru>
2022-12-23 02:07:31 +0500

    router, need to fix app-email-body

--- src/main.js
+++ src/main.js
@@ -1,24 +1,8 @@
 import { createApp } from 'vue'
 import App from './App.vue'
 import './theme.css'
-import translatePlugin from './translatePlugin'
+import router from './router'
 
-const app = createApp(App)
-
-const ru = {
-	app: {
-		title: 'Как работают плагины во Vue?',
-		changeBtn: 'Переключить язык'
-	}
-}
-
-const en = {
-	app: {
-		title: 'How plugins work in Vue?',
-		changeBtn: 'Change language'
-	}
-}
-
-app.use(translatePlugin, {ru, en})
-
-app.mount('#app')
+createApp(App)
+	.use(router)
+	.mount('#app')
---
+++ src/router.js
@@ -0,0 +1,31 @@
+import { createRouter, createWebHistory } from 'vue-router'
+import Login from './views/Login.vue'
+import Forget from './views/Forget.vue'
+import Dashboard from './views/Dashboard.vue'
+import Mail from './views/Mail.vue'
+
+export default createRouter({
+	history: createWebHistory(),
+	routes: [
+		{
+			path: '/login',
+			component: Login,
+			alias: '/'
+		},
+		{
+			path: '/forget',
+			component: Forget
+		},
+		{
+			path: '/dashboard',
+			component: Dashboard
+		},
+		{
+			path: '/mail/:mailId?',
+			component: Mail
+		},
+		
+	],
+	linkActiveClass: 'active',
+	linkExactActiveClass: 'active'
+})
