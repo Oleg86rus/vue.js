628c55fe309ac9346db1f02f0c103a312cebdeb6
oleg086rus <o.pototskiy@yandex.ru>
2022-12-28 00:33:52 +0500

    mappers

--- src/App.vue
+++ src/App.vue
@@ -3,27 +3,34 @@
   <div class="container with-nav">
     <div class="card">
       <h1>Про Vuex</h1>
-      <h2>Счетчик {{ $store.getters.counter }} ({{ $store.getters.doubleCounter }})</h2>
+      <h2>Счетчик {{ counter }} ({{ doubleCounter }})</h2>
       <button class="btn primary" @click="add">Добавить</button>
-      <button class="btn danger" @click="incrementAsync">Добавить 10</button>
+      <button class="btn danger" @click="incrementAsync({ value: 10, delay: 250 })">Добавить 10</button>
     </div>
   </div>
 </template>
 
 <script>
+import {mapGetters, mapMutations, mapActions} from 'vuex'
 import TheNavbar from './components/TheNavbar.vue'
 export default {
   components: {TheNavbar},
+  computed: mapGetters(['counter', 'doubleCounter']),
   methods: {
-    add() {
-      this.$store.commit('increment')
-    },
-    incrementAsync() {
-      this.$store.dispatch('incrementAsync', {
-        value: 10,
-        delay: 250
-      })
-    },
+    ...mapMutations({
+      add: 'increment'
+    }),
+    // add() {
+    //   this.increment
+    //   // this.$store.commit('increment')
+    // },
+    ...mapActions(['incrementAsync']),
+    // incrementAsync() {
+    //   this.$store.dispatch('incrementAsync', {
+    //     value: 10,
+    //     delay: 250
+    //   })
+    // },
     decrement() {
       this.$store.state.counter++
     }
