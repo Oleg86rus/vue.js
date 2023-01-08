99fb72c8759f9f18208723d37cc4dc6a10681c7b
oleg086rus <o.pototskiy@yandex.ru>
2023-01-09 01:04:15 +0500

    vuex

--- src/router.js
+++ src/router.js
@@ -1,12 +1,14 @@
 import {createRouter, createWebHistory} from 'vue-router'
 import AppHomePage from './views/AppHomePage.vue'
 import AppReusablePage from './views/AppReusablePage.vue'
+import VuexPage from './views/VuexPage.vue'
 
 export const router = createRouter({
 	history: createWebHistory(),
 	routes: [
 		{path: '/', component: AppHomePage},
-		{path: '/reusable', component: AppReusablePage}
+		{path: '/reusable', component: AppReusablePage},
+		{path: '/vuex', component: VuexPage}
 	],
 	linkActiveClass: 'active',
 	linkExactActiveClass: 'active'
